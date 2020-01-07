fda6e2b689d898d4e1ec60686febc95f6e33d987
Bartosz Derleta <bartosz@derleta.com>
2020-01-08 00:30:39 +0100

    Fixing decl

--- bridge.h
+++ bridge.h
@@ -10,7 +10,7 @@
 
 void* chc_construct(char* host = NULL, char* username = NULL, char* password = NULL, char* default_database = NULL, long port = 9000);
 void chc_destruct(void* instance);
-void chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_info_cache* fci_cache);
+size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_info_cache* fci_cache);
 
 #endif /* BRIDGE_H */
 
