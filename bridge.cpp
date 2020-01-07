6d101d67750c78583ac535af6ec76ae6c975d1f4
Bartosz Derleta <bartosz@derleta.com>
2020-01-07 23:28:21 +0100

    Debug removal

--- bridge.cpp
+++ bridge.cpp
@@ -30,3 +30,43 @@ void chc_destruct(void* instance) {
 	Client* client = (Client*)instance;
 	delete client;
 }
+
+void chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_info_cache* fci_cache) {
+	Client* client = (Client*)instance;
+	client.Select(string(query), [] (const Block& block)
+	{
+		zval block;
+		array_init(&block);
+		for (size_t i = 0; i < block.GetRowCount(); ++i) {
+			zval row;
+			array_init(&row);
+			for (size_t j = 0; j < block.GetColumnCount(); ++j) {
+				switch (block[j]->Type()) {
+#define CASE(TYPE) case TYPE: add_next_index_long(&row, block[j]->As<ColumnTYPE>()->At(i)); break;
+					CASE(Int8)
+					CASE(Int16)
+					CASE(Int32)
+					CASE(Int64)
+					CASE(UInt8)
+					CASE(UInt16)
+					CASE(UInt32)
+					CASE(UInt64)
+					CASE(Int128)
+					CASE(Decimal)
+					CASE(Decimal32)
+					CASE(Decimal64)
+					CASE(Decimal128)
+#undef CASE(TYPE)
+#define CASE(TYPE) case TYPE: add_next_index_double(&row, block[j]->As<ColumnTYPE>()->At(i)); break;
+					CASE(Float32)
+					CASE(Float64)
+#undef CASE(TYPE)
+					case String:
+					case FixedString:
+						add_next_index_string(&row, block[j]->As<ColumnString>()->At(i), block[j]->As<ColumnString>()->At(i).length(), true);
+				}
+			}
+			add_next_index_zval(&block, &row);
+		}
+	});
+}
