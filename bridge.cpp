410de32ec9a9cece241d9f38ba2ff5b5f2c89ecd
Bartosz Derleta <bartosz@derleta.com>
2020-01-07 22:52:09 +0100

    Debug

--- bridge.cpp
+++ bridge.cpp
@@ -5,19 +5,24 @@ using namespace std;
 using namespace clickhouse;
 
 void* chc_construct(char* host, char* username, char* password, char* default_database, long port) {
-	ClientOptions opts;
-	if (host) 
-		opts.SetHost(string(host));
-	if (username)
-		opts.SetUser(string(username));
-	if (password)
-		opts.SetPassword(string(password));
-	if (default_database)
-		opts.SetDefaultDatabase(string(default_database));
-	if (port)
-		opts.SetPort(port);
-	Client* client = new Client(opts);
-	return (void*)client;
+	try {
+		ClientOptions opts;
+		if (host) 
+			opts.SetHost(string(host));
+		if (username)
+			opts.SetUser(string(username));
+		if (password)
+			opts.SetPassword(string(password));
+		if (default_database)
+			opts.SetDefaultDatabase(string(default_database));
+		if (port)
+			opts.SetPort(port);
+		Client* client = new Client(opts);
+		return (void*)client;
+	} catch (const std::system_error& e) {
+		zend_throw_exception_ex(zend_exception_get_default(), 1 TSRMLS_CC, e.what());
+		return;
+	}
 }
 
 void chc_destruct(void* instance) {
--- bridge.h
+++ bridge.h
@@ -2,7 +2,8 @@
 #define BRIDGE_H
 
 #include <clickhouse/client.h>
-#include<string>
+#include <string>
+#include <system_error>
 
 void* chc_construct(char* host = NULL, char* username = NULL, char* password = NULL, char* default_database = NULL, long port = 9000);
 void chc_destruct(void* instance);
