dac4dc72399da23e3862cac420b90028b484b12d
Bartosz Derleta <bartosz@derleta.com>
2020-01-09 00:47:12 +0100

    Adding exceptions

--- bridge.cpp
+++ bridge.cpp
@@ -116,71 +116,7 @@ size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_
 							add_assoc_null(rowCache[row], colName);	
 						}
 				}
-			}
-			
-#ifdef DUPA			
-				for (size_t j = 0; j < colCount; ++j) {
-					switch (codes[j]) {
-/* TODO: Significantly optimize iteration over block */						
-#ifdef ASSOC 
-						case Type::Code::Int8:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnInt8>()->At(i)); break;
-						case Type::Code::UInt8:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnUInt8>()->At(i)); break;
-						case Type::Code::Int16:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnInt16>()->At(i)); break;
-						case Type::Code::UInt16:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnUInt16>()->At(i)); break;
-						case Type::Code::Int32:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnInt32>()->At(i)); break;
-						case Type::Code::UInt32:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnUInt32>()->At(i)); break;
-						case Type::Code::Int64:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnInt64>()->At(i)); break;
-						case Type::Code::UInt64:
-							add_assoc_long(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnUInt64>()->At(i)); break;
-						case Type::Code::Float32:
-							add_assoc_double(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnFloat32>()->At(i)); break;
-						case Type::Code::Float64: 
-							add_assoc_double(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnFloat64>()->At(i)); break;
-						case Type::Code::String:
-						case Type::Code::FixedString:
-							add_assoc_stringl(&row, dblock.GetColumnName(j).c_str(), dblock[j]->As<ColumnString>()->At(i).c_str(), dblock[j]->As<ColumnString>()->At(i).length()); break;
-						default:
-							add_assoc_null(&row, dblock.GetColumnName(j).c_str());
-#else
-						case Type::Code::Int8:
-							add_next_index_long(&row, dblock[j]->As<ColumnInt8>()->At(i)); break;
-						case Type::Code::UInt8:
-							add_next_index_long(&row, dblock[j]->As<ColumnUInt8>()->At(i)); break;
-						case Type::Code::Int16:
-							add_next_index_long(&row, dblock[j]->As<ColumnInt16>()->At(i)); break;
-						case Type::Code::UInt16:
-							add_next_index_long(&row, dblock[j]->As<ColumnUInt16>()->At(i)); break;
-						case Type::Code::Int32:
-							add_next_index_long(&row, dblock[j]->As<ColumnInt32>()->At(i)); break;
-						case Type::Code::UInt32:
-							add_next_index_long(&row, dblock[j]->As<ColumnUInt32>()->At(i)); break;
-						case Type::Code::Int64:
-							add_next_index_long(&row, dblock[j]->As<ColumnInt64>()->At(i)); break;
-						case Type::Code::UInt64:
-							add_next_index_long(&row, dblock[j]->As<ColumnUInt64>()->At(i)); break;
-						case Type::Code::Float32:
-							add_next_index_double(&row, dblock[j]->As<ColumnFloat32>()->At(i)); break;
-						case Type::Code::Float64: 
-							add_next_index_double(&row, dblock[j]->As<ColumnFloat64>()->At(i)); break;
-						case Type::Code::String:
-						case Type::Code::FixedString:
-							add_next_index_stringl(&row, dblock[j]->As<ColumnString>()->At(i).c_str(), dblock[j]->As<ColumnString>()->At(i).length()); break;
-						default:
-							add_next_index_null(&row);
-#endif
-					}
-				}
-				add_next_index_zval(&block, &row);
-			}
-#endif		
-		
+			}	
 			/* Send to callback */
 			fci->retval = &result;
 			fci->param_count = 1;
@@ -191,6 +127,8 @@ size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_
 		});
 		return total;
 	} catch (const std::system_error& e) {
+		zend_throw_exception_ex(zend_exception_get_default(), 1 TSRMLS_CC, e.what());
+	} catch (const clickhouse::ServerException& e) {
 		zend_throw_exception_ex(zend_exception_get_default(), 1 TSRMLS_CC, e.what());
 	}
 }
