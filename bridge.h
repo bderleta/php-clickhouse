2352594becbff9de9e8626868b01ed46ed94a338
Bartosz Derleta <bartosz@derleta.com>
2020-01-09 19:35:23 +0100

    Enabling config.h

--- bridge.h
+++ bridge.h
@@ -9,6 +9,8 @@
 #include "zend_types.h"
 #include "Zend/zend_exceptions.h"
 
+#include "config.h"
+
 void* chc_construct(char* host = NULL, char* username = NULL, char* password = NULL, char* default_database = NULL, long port = 9000, bool compression = true);
 void chc_destruct(void* instance);
 size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_info_cache* fci_cache);
