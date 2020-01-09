8b7b87114d2fd1f95ed2bb1a1cd88cb892371a6c
Bartosz Derleta <bartosz@derleta.com>
2020-01-09 19:12:55 +0100

    Fixing nullable support

--- bridge.cpp
+++ bridge.cpp
@@ -105,7 +105,38 @@ size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_
 					add_assoc_stringl(&rows[row], colName, colCast->At(row).c_str(), colCast->At(row).length()); \
 				} \
 				break
-				
+#define LOOP_NULLABLE_AS_LONG for (size_t row = 0; row < rowCount; ++row) { \
+				if (outerColCast->IsNull(row)) \
+					add_assoc_null(&rows[row], colName); \
+				else \
+					add_assoc_long(&rows[row], colName, colCast->At(row)); \
+				} \
+				break
+#define LOOP_NULLABLE_AS_DOUBLE for (size_t row = 0; row < rowCount; ++row) { \
+				if (outerColCast->IsNull(row)) \
+					add_assoc_null(&rows[row], colName); \
+				else \
+					add_assoc_double(&rows[row], colName, colCast->At(row)); \
+				} \
+				break
+#define LOOP_NULLABLE_AS_ENUM for (size_t row = 0; row < rowCount; ++row) { \
+				if (outerColCast->IsNull(row)) \
+					add_assoc_null(&rows[row], colName); \
+				else \
+					add_assoc_stringl(&rows[row], colName, colCast->NameAt(row).c_str(), colCast->NameAt(row).length()); \
+				} \
+				break
+#define LOOP_NULLABLE_AS_STRING for (size_t row = 0; row < rowCount; ++row) { \
+				if (outerColCast->IsNull(row)) \
+					add_assoc_null(&rows[row], colName); \
+				else \
+					add_assoc_stringl(&rows[row], colName, colCast->At(row).c_str(), colCast->At(row).length()); \
+				} \
+				break
+#define LOOP_AS_NULL for (size_t row = 0; row < rowCount; ++row) { \
+					add_assoc_null(&rows[row], colName);	
+				}			
+						
 				/**
 				 * TODO: Support for DATE / DATETIME ?
 				 */
@@ -183,102 +214,97 @@ size_t chc_select(void* instance, char* query, zend_fcall_info* fci, zend_fcall_
 					case Type::Code::Nullable:
 					{
 						auto outerColCast = dblock[col]->As<ColumnNullable>();
-						for (size_t row = 0; row < rowCount; ++row) {
-							if (outerColCast->IsNull(row)) {
-								add_assoc_null(&rows[row], colName);	
-							} else {
-								switch (outerColCast->Type()->As<NullableType>()->GetNestedType()->GetCode()) {
-									case Type::Code::Int8:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnInt8>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::UInt8:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnUInt8>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Int16:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnInt16>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::UInt16:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnUInt16>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Int32:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnInt32>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::UInt32:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnUInt32>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Int64:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnInt64>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::UInt64:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnUInt64>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Float32:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnFloat32>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Float64:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnFloat64>();
-										LOOP_AS_LONG;
-									}
-									case Type::Code::Enum8:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnEnum8>();
-										LOOP_AS_ENUM;
-									}
-									case Type::Code::Enum16:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnEnum16>();
-										LOOP_AS_ENUM;
-									}
-									case Type::Code::String:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnString>();
-										LOOP_AS_STRING;
-									}
-									case Type::Code::FixedString:
-									{
-										auto colCast = outerColCast->Nested()->As<ColumnFixedString>();
-										LOOP_AS_STRING;
-									}
-									default:
-									{
-										for (size_t row = 0; row < rowCount; ++row) {
-											add_assoc_null(&rows[row], colName);	
-										}
-									}
-								}
+						switch (outerColCast->Type()->As<NullableType>()->GetNestedType()->GetCode()) {
+							case Type::Code::Int8:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnInt8>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::UInt8:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnUInt8>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Int16:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnInt16>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::UInt16:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnUInt16>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Int32:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnInt32>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::UInt32:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnUInt32>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Int64:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnInt64>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::UInt64:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnUInt64>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Float32:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnFloat32>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Float64:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnFloat64>();
+								LOOP_NULLABLE_AS_LONG;
+							}
+							case Type::Code::Enum8:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnEnum8>();
+								LOOP_NULLABLE_AS_ENUM;
+							}
+							case Type::Code::Enum16:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnEnum16>();
+								LOOP_NULLABLE_AS_ENUM;
+							}
+							case Type::Code::String:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnString>();
+								LOOP_NULLABLE_AS_STRING;
+							}
+							case Type::Code::FixedString:
+							{
+								auto colCast = outerColCast->Nested()->As<ColumnFixedString>();
+								LOOP_NULLABLE_AS_STRING;
+							}
+							default:
+							{
+								LOOP_AS_NULL;
 							}
 						}
 					}
 					default:
 					{
-						for (size_t row = 0; row < rowCount; ++row) {
-							add_assoc_null(&rows[row], colName);	
-						}
+						LOOP_AS_NULL;
 					}
 				}
 #undef LOOP_AS_LONG
 #undef LOOP_AS_DOUBLE
 #undef LOOP_AS_ENUM
 #undef LOOP_AS_STRING
+#undef LOOP_NULLABLE_AS_LONG
+#undef LOOP_NULLABLE_AS_DOUBLE
+#undef LOOP_NULLABLE_AS_ENUM
+#undef LOOP_NULLABLE_AS_STRING				
+#undef LOOP_AS_NULL
 			}	
 			/* Send to callback */
 			fci->retval = &result;
